188e8fe4d0b350719a69f0ad1e28c47e05c925fb
Hamza El Bouatmani <hamza@mon-pc.local>
2018-02-28 10:53:19 +0000

    added front-end validation and error box in forms

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -9,7 +9,8 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logged: false
+      logged: false,
+      error: ""
     };
     this.submitHandler = this.submitHandler.bind(this);
   }
@@ -28,10 +29,16 @@ class Login extends Component {
 
   submitHandler(ev) {
     ev.preventDefault();
-    console.log('Submitting form ...');
+
     let email = this.emailInput.value;
     let password = this.passwordInput.value;
     console.log(email,password);
+
+    if (password.length < 8) {
+      this.passwordInput.value = "";
+      this.passwordInput.focus();
+      return;
+    }
 
     let payload = JSON.stringify({ email: email, password: password });
     fetch ('http://localhost:3000/api/v1/users/sign-in/', {
@@ -47,12 +54,20 @@ class Login extends Component {
           console.log('data of login in :');
           console.log(data);
           localStorage.setItem('token', data.token);
+          this.setState({
+            logged: true,
+            error: ""
+          });
           this.props.history.push('/shops/nearby');
         }).catch( (err) => {
           console.log('problem in jsonifying login response');
         });
       } else {
         console.error('User with given credentials Not authorized by the server !');
+        this.setState({
+          ...this.state,
+          error: "Wrong credentials"
+        });
       }
     } )
     .catch ( (err) => { console.error('Error in Login Fetch ...'); } );
@@ -67,16 +82,19 @@ class Login extends Component {
         <form onSubmit={this.submitHandler}>
             <div className="field">
               <label htmlFor="email">E-mail: </label>
-              <input type="text" name="email" ref={(input) => {this.emailInput = input; }}/>
+              <input type="email" name="email" required placeholder="valid e-mail" ref={(input) => {this.emailInput = input; }}/>
             </div>
             <div className="field">
               <label htmlFor="password">Password: </label>
-              <input type="password" name="password" ref={(input) => {this.passwordInput = input; }} />
+              <input type="password" name="password" required placeholder="( at lease 8 characters )" ref={(input) => {this.passwordInput = input; }} />
             </div>
             <div className="field">
               <button type="submit">Sign-in</button>
             </div>
         </form>
+        <div className={`error-msg ${this.state.error === "" ? 'hidden' : ''}`}>
+          <p>{this.state.error}</p>
+        </div>
       {localStorage.getItem('token') ? <Redirect from="/signin" to="/shops/nearby" /> : null}
       </div>
     );
--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -9,6 +9,7 @@ class Register extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: ""
     };
     this.submitHandler = this.submitHandler.bind(this);
   }
@@ -23,6 +24,12 @@ class Register extends Component {
     let name = this.nameInput.value;
     let email = this.emailInput.value;
     let password = this.passwordInput.value;
+
+    if (password.length < 8) {
+      this.passwordInput.value = "";
+      this.passwordInput.focus();
+      return;
+    }
 
     let payload = JSON.stringify({ name: name, email: email, password: password });
     fetch ('http://localhost:3000/api/v1/users/sign-up/', {
@@ -43,6 +50,9 @@ class Register extends Component {
         });
       } else {
         console.error('Not authorized !');
+        this.setState({
+          error: "Server could not register the user with the provided info"
+        });
       }
     } )
     .catch ( (err) => { console.error('Error fetching ...'); } );
@@ -56,20 +66,23 @@ class Register extends Component {
         <form onSubmit={this.submitHandler}>
             <div className="field">
               <label htmlFor="name">Name: </label>
-              <input type="text" name="name" ref={(input) => {this.nameInput = input; }}/>
+              <input type="text" name="name" required placeholder="name" ref={(input) => {this.nameInput = input; }}/>
             </div>
             <div className="field">
               <label htmlFor="email">E-mail: </label>
-              <input type="text" name="email" ref={(input) => {this.emailInput = input; }} />
+              <input type="email" name="email" required placeholder="valid e-mail" ref={(input) => {this.emailInput = input; }} />
             </div>
             <div className="field">
               <label htmlFor="password">Password: </label>
-              <input type="password" name="password" ref={(input) => {this.passwordInput = input; }} />
+              <input type="password" name="password" required placeholder="( at lease 8 characters )" ref={(input) => {this.passwordInput = input; }} />
             </div>
             <div className="field">
-              <button type="submit">Sign-in</button>
+              <button type="submit">Sign-up</button>
             </div>
         </form>
+        <div className={`error-msg ${this.state.error === "" ? 'hidden' : ''}`}>
+          <p>{this.state.error}</p>
+        </div>
       {localStorage.getItem('token') ? <Redirect from="/signup" to="/shops/nearby" /> : null}
       </div>
 
